ab4a38399f3bad262ca428128c11de9a1dbb2073
yrsegal <yrsegal@gmail.com>
2017-06-30 12:38:14 +0300

    bump version so it's different

--- src/main/java/shadowfox/botanicaladdons/api/lib/LibMisc.java
+++ src/main/java/shadowfox/botanicaladdons/api/lib/LibMisc.java
@@ -8,7 +8,7 @@ public final class LibMisc {
     public static final String MOD_NAME = "Natural Pledge";
     public static final String MOD_ID = "botanicaladdons";
 
-    public static final String BUILD = "2";
+    public static final String BUILD = "3";
     public static final String VERSIONID = "2";
     public static final String VERSION = VERSIONID + "." + BUILD;
     public static final String DEPENDENCIES = "required-after:forge@[13.20.0.2259,);required-after:botania;required-after:librarianlib;";
