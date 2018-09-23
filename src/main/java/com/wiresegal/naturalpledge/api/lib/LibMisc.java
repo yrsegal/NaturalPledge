23695b165cae82136ce570d8ab5d8fa4e64826a4
Unknown <dootydooter@gmail.com>
2018-09-23 05:29:10 -0500

    Version bump to 3.0

--- src/main/java/com/wiresegal/naturalpledge/api/lib/LibMisc.java
+++ src/main/java/com/wiresegal/naturalpledge/api/lib/LibMisc.java
@@ -8,10 +8,10 @@ public final class LibMisc {
     public static final String MOD_NAME = "Natural Pledge";
     public static final String MOD_ID = "naturalpledge";
 
-    public static final String BUILD = "3";
-    public static final String VERSIONID = "2";
+    public static final String BUILD = "0";
+    public static final String VERSIONID = "3";
     public static final String VERSION = VERSIONID + "." + BUILD;
-    public static final String DEPENDENCIES = "required-after:forge@[13.20.0.2259,);required-after:botania;required-after:librarianlib;";
+    public static final String DEPENDENCIES = "required-after:forge@[14.23.4.2705,);required-after:botania;required-after:librarianlib;";
 
     public static final String PROXY_COMMON = "com.wiresegal.naturalpledge.common.core.CommonProxy";
     public static final String PROXY_CLIENT = "com.wiresegal.naturalpledge.client.core.ClientProxy";
